307ebaed988fdd8272b021bbe65ef7e39c136515
Melissa Sperry <sperry.mel@gmail.com>
2020-02-22 07:09:16 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -15,8 +15,7 @@ def get_the_values(groceries)
     meat: ["chicken", "steak", "salmon"],
    grains: ["rice", "pasta"]
    }
-   groceries = {fruit: "Banana", vegetable: "Broccoli", dessert: "Cookie"}
-
+  
 groceries.values
 
 end
